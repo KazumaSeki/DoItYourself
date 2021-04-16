c48a748340bee86ad6ccfed586fb115e1062411d
KazumaSeki <s.kazuma1996402@gmail.com>
2021-04-16 16:11:00 +0900

    fix_config

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  root to: "recipes#index"
+
+  resources :users, only: %i(index show edit update)
+
+  resources :recipes do
+    resource :likes, only: %i(create destroy)
+    resource :comments, only: %i(create destroy)
+  end
+end
